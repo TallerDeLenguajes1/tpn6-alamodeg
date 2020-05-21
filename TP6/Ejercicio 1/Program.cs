68d80e8e575a9a2190702a0c6c301abaa9ee5119
alamodeg <alvarodeguer@gmail.com>
2020-05-20 23:02:42 -0300

    Creado .gitignore y listo Ejercicio 1

---
+++ TP6/Ejercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,resto,reverso = 0;
+            do
+            {
+                Console.WriteLine("Ingrese un numero POSITIVO para mostrar su reverso: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }while ( num <= 0);
+
+            Console.WriteLine("El numero ingresado es " + num);
+            while (num >0)
+            {
+                resto = num % 10;
+                num = num / 10;
+                reverso = (reverso + resto) * 10;
+            }
+            Console.WriteLine("El reverso del numero es " + reverso);
+        }
+    }
+}
